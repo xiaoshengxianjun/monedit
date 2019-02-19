5f7a23159965ba0933db00d75483fbeed9958e5c
MSI\xsxjl <xsxjlzy@163.com>
2019-02-19 18:04:47 +0800

    初始化项目，修改首页内容

---
+++ src/renderer/components/input/input.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="input-wrapper">
+        <label>邮箱：</label>
+        <input type="text" placeholder="请输入邮箱">
+    </div>
+</template>
+<style lang="scss" scoped>
+.input-wrapper{
+    input{
+        width: 200px;
+        height: 40px;
+    }
+}
+</style>
---
+++ src/renderer/pages/LandingPage.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="landing-page">
+    <div class="login-wrapper">
+      <input-component></input-component>
+    </div>
+    <div class="news-wrapper">新闻信息页</div>
+  </div>
+</template>
+<script>
+import InputComponent from "@/components/input/input"
+
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    InputComponent
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.landing-page {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  left: 0px;
+  top: 0px;
+  overflow: hidden;
+  .login-wrapper {
+    width: 30%;
+    height: 100%;
+    background: red;
+    float: left;
+  }
+  .news-wrapper{
+    width: 70%;
+    height: 100%;
+    background: blue;
+    float: right;
+  }
+}
+</style>
